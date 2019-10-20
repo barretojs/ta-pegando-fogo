d69848ab97573a1b2e446a5ed139db5a45af5a59
rafael.landim <rafael.landim@agilecontent.com>
2019-10-20 10:34:30 -0300

    initial backend

---
+++ backend/SpaceAppsChallenge.CMS/ViewModels/ReceiverViewModel.cs
@@ -0,0 +1,42 @@
+﻿using SpaceAppsChallenge.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpaceAppsChallenge.CMS.ViewModels
+{
+    public class ReceiverViewModel
+    {
+
+        public string Name { get; set; }
+        public string Telephony { get; set; }
+        public int Type { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public float OperatingRange { get; set; }
+        public string ApiKey { get; set; }
+
+        public ReceiverViewModel()
+        {
+
+        }
+
+        public ReceiverViewModel(Receiver receiver)
+        {
+            this.Name = receiver.Name;
+            this.Telephony = receiver.Telephony;
+            this.Type = receiver.Type;
+            this.Latitude = receiver.Position.Latitude;
+            this.Longitude = receiver.Position.Longitude;
+            this.OperatingRange = receiver.OperatingRange;
+                 
+        }
+
+        public Receiver ToReceiver()
+        {
+            Receiver result = new Receiver(this.Name, this.Telephony, this.Latitude, this.Longitude, this.OperatingRange);
+            return result;
+        }
+    }
+}
---
+++ backend/SpaceAppsChallenge.Model/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceAppsChallenge.Model
+{
+    public class Receiver
+    {
+        public string Name { get; set; }
+        public string Telephony { get; set; }
+        public int Type { get; set; }
+        public Position Position { get; set; }
+        public float OperatingRange { get; set; }
+
+        public Receiver(string name, string telephony, float latitude, float longitude, float operationRange)
+        {
+            this.Name = name;
+            this.Telephony = telephony;
+            this.Position = new Position(latitude, longitude);
+            this.OperatingRange = operationRange;
+        }
+
+    }
+}
